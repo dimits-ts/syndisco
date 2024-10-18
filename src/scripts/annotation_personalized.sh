88897cfdba2bf59c41e3d1eb7a7beeeddeb90c7e
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2024-10-18 11:20:27 +0300

    add logs to framework

--- src/scripts/annotation_personalized.sh
+++ src/scripts/annotation_personalized.sh
@@ -3,6 +3,8 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SRC_DIR="$(dirname "$SCRIPT_DIR")"
 PROJECT_ROOT_DIR="$(dirname "$SRC_DIR")"
+LOG_DIR="$PROJECT_ROOT_DIR/logs"
+CURRENT_DATE=$(date +'%Y-%m-%d')
 
 PYTHON_SCRIPT_PATH="$SCRIPT_DIR/sdf_create_annotations.py"
 OUTPUT_DIR="$PROJECT_ROOT_DIR/data/annotations_output"
@@ -10,6 +12,7 @@ MODEL_PATH="$SRC_DIR/models/llama-3-8B.gguf"
 CONV_INPUT_DIR="$PROJECT_ROOT_DIR/generated_discussions_output"
 ANNOTATOR_PROMPT_DIR="$PROJECT_ROOT_DIR/data/annotations_input"
 
+mkdir -p "$LOG_DIR"
 
 # for each synthetic conversation
 for DIR in "$CONV_INPUT_DIR"/*; do
@@ -20,6 +23,7 @@ for DIR in "$CONV_INPUT_DIR"/*; do
         --conv_input_dir "$DIR" \
         --prompt_path "$ANNOTATOR_PROMPT_PATH" \
         --output_dir "$OUTPUT_DIR" \
-        --model_path "$MODEL_PATH"
+        --model_path "$MODEL_PATH" \
+        2>&1 | tee -a "$LOG_DIR/$CURRENT_DATE.txt"
     done
 done
--- src/scripts/conversation_personalized.sh
+++ src/scripts/conversation_personalized.sh
@@ -3,6 +3,10 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SRC_DIR="$(dirname "$SCRIPT_DIR")"
 PROJECT_ROOT_DIR="$(dirname "$SRC_DIR")"
+LOG_DIR="$PROJECT_ROOT_DIR/logs"
+CURRENT_DATE=$(date +'%Y-%m-%d')
+
+mkdir -p "$LOG_DIR"
 
 bash "$SCRIPT_DIR/conversation_execute_all.sh" \
     --python_script_path  "$SRC_DIR/sdf_create_conversations.py" \
@@ -11,4 +15,5 @@ bash "$SCRIPT_DIR/conversation_execute_all.sh" \
     --model_path "$SRC_DIR/models/llama-3-8B-instruct.gguf" \
     --max_tokens 500 \
     --ctx_width_tokens 2048 \
-    --gpu_layers 10
+    --gpu_layers 10  \
+    2>&1 | tee -a "$LOG_DIR/$CURRENT_DATE.txt"
