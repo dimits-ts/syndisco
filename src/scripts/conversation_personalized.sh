a9ca001425ef0686de3c4a0db91f533f2c47108e
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2024-10-20 10:48:16 +0300

    vanilla no moderator experiment

--- src/scripts/conversation_personalized.sh
+++ src/scripts/conversation_personalized.sh
@@ -11,10 +11,10 @@ mkdir -p "$LOG_DIR"
 bash "$SCRIPT_DIR/conversation_execute_all.sh" \
     --python_script_path  "$SRC_DIR/sdf_create_conversations.py" \
     --input_dir "$PROJECT_ROOT_DIR/data/generated_discussions_input/conv_data/generated" \
-    --output_dir "$PROJECT_ROOT_DIR/data/generated_discussions_output" \
+    --output_dir "$PROJECT_ROOT_DIR/data/generated_discussions_output/vanilla_no_mod" \
     --model_path "$SRC_DIR/models/llama-3-8B-instruct.gguf" \
     --max_tokens 500 \
     --ctx_width_tokens 2048 \
-    --inference_threads 8 \
+    --inference_threads 10 \
     --gpu_layers 10  \
     2>&1 | tee -a "$LOG_DIR/$CURRENT_DATE.txt"
