8d866ba188ec6d2954c9f5b10265df79b72e10bb
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2024-11-05 16:11:59 +0200

    rename sdf python scripts and hardcode them in bash scripts

--- src/scripts/annotation_execute_all.sh
+++ src/scripts/annotation_execute_all.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+THIS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PROJECT_ROOT_DIR="$(dirname "$THIS_DIR")"
+SCRIPT_PATH="$PROJECT_ROOT_DIR/generate_annotations.py"
+
 usage() {
   echo "Usage: $0 --python_script_path <python script path> --conv_input_dir <input_directory> --prompt_path <input_path> --output_dir <output_directory> --model_path <model_file_path>"
   exit 1
@@ -8,7 +12,6 @@ usage() {
 
 while [[ "$#" -gt 0 ]]; do
   case $1 in
-    --python_script_path) python_script_path="$2"; shift;;
     --conv_input_dir) conv_input_dir="$2"; shift ;;
     --prompt_path) prompt_path="$2"; shift ;;
     --output_dir) output_dir="$2"; shift ;;
@@ -19,15 +22,9 @@ while [[ "$#" -gt 0 ]]; do
 done
 
 # Check if all required arguments are provided
-if [[ -z "$python_script_path" || -z "$conv_input_dir" || -z "$prompt_path" || -z "$output_dir" || -z "$model_path" ]]; then
+if [[-z "$conv_input_dir" || -z "$prompt_path" || -z "$output_dir" || -z "$model_path" ]]; then
   echo "Error: Missing required arguments."
   usage
-fi
-
-# Check if model path exists
-if [[ ! -f "$python_script_path" ]]; then
-  echo "Error: Python source path '$python_script_path' does not exist."
-  exit 1
 fi
 
 if [[ ! -d "$conv_input_dir" ]]; then
@@ -48,7 +45,7 @@ fi
 for input_file in "$conv_input_dir"/*; do
   if [[ -f "$input_file" ]]; then
     echo "Processing file: $input_file"
-    python -u "$python_script_path" --prompt_input_path="$prompt_path" --conv_path="$input_file" --output "$output_dir" --model_path "$model_path" --ctx_width_tokens=2048 --gpu_layers 8
+    python -u "$SCRIPT_PATH" --prompt_input_path="$prompt_path" --conv_path="$input_file" --output "$output_dir" --model_path "$model_path" --ctx_width_tokens=2048 --gpu_layers 8
   else
     echo "Skipping non-file entry: $input_file"
   fi
--- src/scripts/conversation_execute_all.sh
+++ src/scripts/conversation_execute_all.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+THIS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PROJECT_ROOT_DIR="$(dirname "$THIS_DIR")"
+SCRIPT_PATH="$PROJECT_ROOT_DIR/generate_conversations.py"
+
 usage() {
   echo "Usage: $0 --python_script_path <python script path> --input_dir <input_directory> --output_dir <output_directory> --model_path <model_file_path>"
   exit 1
@@ -8,7 +12,6 @@ usage() {
 
 while [[ "$#" -gt 0 ]]; do
   case $1 in
-    --python_script_path) python_script_path="$2"; shift ;;
     --input_dir) input_dir="$2"; shift ;;
     --output_dir) output_dir="$2"; shift ;;
     --model_path) model_path="$2"; shift ;;
@@ -22,15 +25,9 @@ while [[ "$#" -gt 0 ]]; do
 done
 
 # Check if all required arguments are provided
-if [[ -z "$python_script_path" || -z "$input_dir" || -z "$output_dir" || -z "$model_path" ]]; then
+if [[ || -z "$input_dir" || -z "$output_dir" || -z "$model_path" ]]; then
   echo "Error: Missing required arguments."
   usage
-fi
-
-# Check if model path exists
-if [[ ! -f "$python_script_path" ]]; then
-  echo "Error: Python source path '$python_script_path' does not exist."
-  exit 1
 fi
 
 if [[ ! -d "$input_dir" ]]; then
@@ -47,7 +44,7 @@ fi
 for input_file in "$input_dir"/*; do
   if [[ -f "$input_file" ]]; then
     echo "Processing file: $input_file"
-    python -u "$python_script_path" \
+    python -u "$SCRIPT_PATH" \
           --output "$output_dir" \
           --model_path "$model_path" \
           --input_file="$input_file" \
