f1149be598a52be51f5789f0818b5e5362f5d440
Dimitris <dimits-ts>
2025-01-03 11:49:53 +0200

    fix transformer model responses

--- src/sdl/backend/trans_model.py
+++ src/sdl/backend/trans_model.py
@@ -28,11 +28,12 @@ class TransformersModel(model.Model):
         """
         super().__init__(name, max_out_tokens, remove_string_list)
 
-        model = transformers.AutoModelForCausalLM.from_pretrained(
-            model_path
-        )
-        tokenizer = transformers.AutoTokenizer.from_pretrained(model_path)
+        model = transformers.AutoModelForCausalLM.from_pretrained(model_path, device_map="auto")
 
+        print("Model memory footprint: ", model.get_memory_footprint())
+
+        tokenizer = transformers.AutoTokenizer.from_pretrained(model_path)
+        
         self.generator = transformers.pipeline(
             "text-generation", model=model, tokenizer=tokenizer
         )
@@ -40,4 +41,4 @@ class TransformersModel(model.Model):
     def generate_response(
         self, json_prompt: tuple[typing.Any, typing.Any], stop_words: list[str]
     ) -> str:
-        return self.generator(json_prompt, max_length=self.max_out_tokens, return_full_text=False)  # type: ignore
+        return self.generator(json_prompt, max_length=self.max_out_tokens, return_full_text=False)[0]["generated_text"]
