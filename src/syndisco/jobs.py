60f488547b14dc5c1c2c8ce697139364025dfaae
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2025-04-01 13:54:41 +0300

    fix linting errors

--- src/syndisco/jobs.py
+++ src/syndisco/jobs.py
@@ -34,11 +34,14 @@ from .util import output_util, file_util
 logger = logging.getLogger(Path(__file__).name)
 
 
-# No superclass because the shared method names between the classes is coincidental
+# No superclass because the shared method names between the classes 
+# is coincidental
+
 
 class Discussion:
     """
-    A job conducting a discussion between different actors (:class:`actors.Actor`).
+    A job conducting a discussion between different actors 
+    (:class:`actors.Actor`).
     """
 
     def __init__(
@@ -54,7 +57,8 @@ class Discussion:
         """
         Construct the framework for a conversation to take place.
 
-        :param turn_manager: an object handling the speaker priority of the users
+        :param turn_manager: an object handling the speaker priority of the
+        participants
         :type turn_manager: turn_manager.TurnManager
         :param users: A list of discussion participants
         :type users: list[actors.Actor]
@@ -80,18 +84,24 @@ class Discussion:
         """
         # just to satisfy the type checker
         self.next_turn_manager = next_turn_manager
+
         self.username_user_map = {user.get_name(): user for user in users}
-        # used during export, in order to keep information about the underlying models
-        self.user_types = [type(user).__name__ for user in self.username_user_map]
+
+        # used only during export, tags underlying models
+        self.user_types = [
+            type(user).__name__ for user in self.username_user_map
+        ]
+
         self.moderator = moderator
         self.conv_len = conv_len
-        # unique id for each conversation, generated for persistence purposes
+
+        # unique id for each conversation
         self.id = uuid.uuid4()
 
-        self.conv_logs = []
         # keep a limited context of the conversation to feed to the models
         self.ctx_len = history_context_len
         self.ctx_history = collections.deque(maxlen=history_context_len)
+        self.conv_logs = []
 
         self.seed_opinion_user = seed_opinion_user
         self.seed_opinion = seed_opinion
@@ -102,11 +112,13 @@ class Discussion:
         :param verbose: whether to print the messages on the screen
         as they are generated, defaults to True
         :type verbose: bool, optional
-        :raises RuntimeError: if the object has already been used to generate a conversation
+        :raises RuntimeError: if the object has already been used to generate 
+        a conversation
         """
         if len(self.conv_logs) != 0:
             raise RuntimeError(
-                "This conversation has already been concluded, create a new Conversation object."
+                "This conversation has already been concluded, "
+                "create a new Discussion object."
             )
 
         if self.seed_opinion.strip() != "":
@@ -119,7 +131,9 @@ class Discussion:
                 instructions="",
                 actor_type=actors.ActorType.USER,
             )
-            self._archive_response(seed_user, self.seed_opinion, verbose=verbose)
+            self._archive_response(
+                seed_user, self.seed_opinion, verbose=verbose
+            )
         else:
             logger.info("No seed opinion provided.")
 
@@ -138,12 +152,15 @@ class Discussion:
                     res = self.moderator.speak(list(self.ctx_history))
                     self._archive_response(self.moderator, res, verbose)
 
-    def to_dict(self, timestamp_format: str = "%y-%m-%d-%H-%M") -> dict[str, Any]:
+    def to_dict(
+        self, timestamp_format: str = "%y-%m-%d-%H-%M"
+    ) -> dict[str, Any]:
         """
-        Get a dictionary view of the data and metadata contained in the conversation.
+        Get a dictionary view of the data and metadata contained in the 
+        discussion.
 
-        :param timestamp_format: the format for the conversation's creation time,
-         defaults to "%y-%m-%d-%H-%M"
+        :param timestamp_format: the format for the conversation's creation 
+        time, defaults to "%y-%m-%d-%H-%M"
         :type timestamp_format: str, optional
         :return: a dict representing the conversation
         :rtype: dict[str, Any]
@@ -151,15 +168,21 @@ class Discussion:
         return {
             "id": str(self.id),
             "timestamp": datetime.datetime.now().strftime(timestamp_format),
-            "users": [user.get_name() for user in self.username_user_map.values()],
+            "users": [
+                user.get_name() for user in self.username_user_map.values()
+            ],
             "moderator": (
-                self.moderator.get_name() if self.moderator is not None else None
+                self.moderator.get_name()
+                if self.moderator is not None
+                else None
             ),
             "user_prompts": [
                 user.describe() for user in self.username_user_map.values()
             ],
             "moderator_prompt": (
-                self.moderator.describe() if self.moderator is not None else None
+                self.moderator.describe()
+                if self.moderator is not None
+                else None
             ),
             "ctx_length": self.ctx_len,
             "logs": self.conv_logs,
@@ -168,7 +191,6 @@ class Discussion:
     def to_json_file(self, output_path: str | Path) -> None:
         """
         Export the data and metadata of the conversation as a json file.
-        Convenience function equivalent to json.dump(self.to_dict(), output_path)
 
         :param output_path: the path for the exported file
         :type output_path: str
@@ -231,8 +253,8 @@ class Discussion:
 
 class Annotation:
     """
-    An annotation job modelled as a discussion between the system writing the logs of 
-    a finished discussion, and the LLM Annotator.
+    An annotation job modelled as a discussion between the system writing the
+    logs of a finished discussion, and the LLM Annotator.
     """
 
     def __init__(
@@ -243,16 +265,20 @@ class Annotation:
         history_ctx_len: int = 2,
     ):
         """Create an annotation job.
-        The annotation is modelled as a conversation between the system and the annotator.
+        The annotation is modelled as a conversation between the system and 
+        the annotator.
 
         :param annotator: The annotator
         :type annotator: actors.IActor
-        :param conv_logs_path: The path to the file containing the conversation logs in JSON format
+        :param conv_logs_path: The path to the file containing the 
+        discussion logs in JSON format
         :type conv_logs_path: str | Path
-        :param include_moderator_comments: Whether to annotate moderator comments, and include them 
+        :param include_moderator_comments: Whether to annotate moderator 
+        comments, and include them
         in conversational context when annotating user responses.
         :type include_moderator_comments: bool
-        :param history_ctx_len: How many previous comments the annotator will remember, defaults to 4
+        :param history_ctx_len: How many previous comments the annotator will 
+        remember, defaults to 4
         :type history_ctx_len: int, optional
         """
         self.annotator = annotator
@@ -267,7 +293,8 @@ class Annotation:
         """
         Begin the conversation-modelled annotation job.
 
-        :param verbose: whether to print the results of the annotation to the console, defaults to True
+        :param verbose: whether to print the results of the annotation to the 
+        console, defaults to True
         :type verbose: bool, optional
         """
         ctx_history = collections.deque(maxlen=self.history_ctx_len)
@@ -276,12 +303,14 @@ class Annotation:
             username = message_data["name"]
             message = message_data["text"]
 
-            # do not include moderator comments in annotation context if told so
+            # do not include moderator comments in annotation ctx if told so
             if "moderator" in username:
                 if not self.include_moderator_comments:
                     continue
 
-            formatted_message = output_util.format_chat_message(username, message)
+            formatted_message = output_util.format_chat_message(
+                username, message
+            )
             ctx_history.append(formatted_message)
             annotation = self.annotator.speak(list(ctx_history))
             self.annotation_logs.append((message, annotation))
@@ -290,11 +319,15 @@ class Annotation:
                 print(textwrap.fill(formatted_message))
                 print(annotation)
 
-    def to_dict(self, timestamp_format: str = "%y-%m-%d-%H-%M") -> dict[str, Any]:
+    def to_dict(
+        self, timestamp_format: str = "%y-%m-%d-%H-%M"
+    ) -> dict[str, Any]:
         """
-        Get a dictionary view of the data and metadata contained in the conversation.
+        Get a dictionary view of the data and metadata contained in 
+        the annotation.
 
-        :param timestamp_format: the format for the conversation's creation time, defaults to "%y-%m-%d-%H-%M"
+        :param timestamp_format: the format for the conversation's creation 
+        time, defaults to "%y-%m-%d-%H-%M"
         :type timestamp_format: str, optional
         :return: a dict representing the conversation
         :rtype: dict[str, Any]
@@ -311,7 +344,6 @@ class Annotation:
     def to_json_file(self, output_path: str | Path) -> None:
         """
         Export the data and metadata of the conversation as a json file.
-        Convenience function equivalent to json.dump(self.to_dict(), output_path)
 
         :param output_path: the path for the exported file
         :type output_path: str
