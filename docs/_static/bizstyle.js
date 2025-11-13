3bbf38d1afcbbd01caef871ad76643e50a13b270
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2025-11-13 10:26:53 +0200

    update doc

--- docs/_static/bizstyle.js
+++ docs/_static/bizstyle.js
@@ -17,7 +17,7 @@ const initialiseBizStyle = () => {
 }
 
 window.addEventListener("resize",
-  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "SynDisco 1.0.0 documentation")
+  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "SynDisco 2.0.0 documentation")
 )
 
 if (document.readyState !== "loading") initialiseBizStyle()
--- sphinx/conf.py
+++ sphinx/conf.py
@@ -23,7 +23,7 @@ copyright = "2025, Dimitris Tsirmpas"
 author = "Dimitris Tsirmpas"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.0"
+release = "2.0.0"
 
 
 # -- General configuration ---------------------------------------------------
