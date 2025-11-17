1b228e80598fc062cfcffa55037b99cfefa72984
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2025-11-17 14:40:53 +0200

    make licencing compatible with linter

--- src/syndisco/actors.py
+++ src/syndisco/actors.py
@@ -2,25 +2,24 @@
 Module defining LLM users in discussions and their characteristics.
 """
 
-"""
-SynDisco: Automated experiment creation and execution using only LLM agents
-Copyright (C) 2025 Dimitris Tsirmpas
+# SynDisco: Automated experiment creation and execution using only LLM agents
+# Copyright (C) 2025 Dimitris Tsirmpas
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-You may contact the author at tsirbasdim@gmail.com
-"""
+# You may contact the author at dim.tsirmpas@aueb.gr
+
 
 import typing
 import dataclasses
