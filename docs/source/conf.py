f5beba3f9db40d0f414fb171f610c428be127f2b
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2025-12-05 16:20:03 +0200

    fix sphinx configs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2025, Dimitris Tsirmpas"
 author = "Dimitris Tsirmpas"
 
 # The full version, including alpha/beta/rc tags
-release = "2.0.4"
+release = "2.0.5"
 
 
 # -- General configuration ---------------------------------------------------
@@ -31,7 +31,7 @@ release = "2.0.4"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "myst_parser", "nbsphinx"]
+extensions = ["sphinx.ext.autodoc", "nbsphinx"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
